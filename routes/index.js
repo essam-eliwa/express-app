d81e223a94b004074e4c49fb7e1dc5cfccd067f6
Essam Eliwa <essam.eliwa@miuegypt.edu.eg>
2023-05-13 22:24:41 +0300

    adding simple js code for testing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Express' });
+  res.render('pages/index', { title: 'SWE230: Express Demo App' , message : 'WE LOVE MERN STACK'});
 });
 
 export default router;
